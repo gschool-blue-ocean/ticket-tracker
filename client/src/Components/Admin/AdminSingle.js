ef7e88b8e37d7450376e58b2cb9664c8f9f30a61
Bwade808 <bwka85@gmail.com>
2022-12-12 13:34:21 -0700

    edited Ticket History page

--- client/src/Components/Admin/AdminSingle.js
+++ client/src/Components/Admin/AdminSingle.js
@@ -1,15 +1,20 @@
 
 
-const AdminSingle = ({ elem }) => {
+const AdminSingle = ({ elem, key }) => {
     return (
         <div className='single-ticket'>
             <div className="ticket-div">{elem.ticket_id}</div>
             <div className="ticket-div">{elem.name}</div>
             <div className="ticket-div">{elem.category}</div>
             <div className={elem.priority === '1' ? "ticket-pri-red" 
-                : elem.priority === '2' ? "ticket-pri-yellow" 
+                : elem.priority === '2' ? "ticket-pri-orange" 
                 : elem.priority === '3' ? "ticket-pri-yellow" 
-                : elem.priority === '4' ? "ticket-pri-green" : null}>{elem.priority}</div>
+                : elem.priority === '4' ? "ticket-pri-green" : null}>
+                    {elem.priority === "1" ? 'Urgent' 
+                        : elem.priority === "2" ? 'Priority'
+                        : elem.priority === "3" ? 'Routine'
+                        : elem.priority === "4" ? 'Minor'
+                    : null}</div>
             <div className="ticket-div">{elem.status}</div>
             <div className="ticket-div">{elem.eta}</div>
         </div>
